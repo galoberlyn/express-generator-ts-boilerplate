519165a5faa19f6ed1114177d057f98f53ddd8d5
galoberlyn <galoberlyn@Galos-MacBook-Pro.local>
2022-10-11 15:08:21 +0800

    initialize repo

---
+++ app.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import path from 'path';
+import logger from 'morgan';
+
+var indexRouter = require('./routes/index');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+export default app;
